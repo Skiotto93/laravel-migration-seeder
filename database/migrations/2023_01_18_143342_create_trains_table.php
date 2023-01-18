8b935053e3248849593576a20813c53c044cd628
skiotto93 <fabio.forenza@hotmail.com>
2023-01-18 15:54:56 +0100

    Aggiunta migration trains e tabelle

--- database/migrations/2023_01_18_143342_create_trains_table.php
+++ database/migrations/2023_01_18_143342_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 60);
+            $table->string('departure_station', 80);
+            $table->string('arrival_staion', 80);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 20);
+            $table->unsignedTinyInteger('wagons');
+            $table->boolean('is_in_time')->default(true);  //dentro default si può indicare rispettivamente true o false con 1 e 0 
+            $table->boolean('is_cancelled')->default(false);
             $table->timestamps();
         });
     }
